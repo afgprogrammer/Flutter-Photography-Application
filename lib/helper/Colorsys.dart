a078c8a0a43475654d3f8a47f105c1f29b4a2630
Mohammad Rahmani <rto1680@gmail.com>
2020-05-21 18:50:15 +0430

    Add coloring system class

---
+++ lib/helper/Colorsys.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Colorsys {
+  static Color orange = Color.fromRGBO(243, 99, 67, 1);
+  static Color grey = Color.fromRGBO(112, 112, 112, 1);
+  static Color grey300 = Colors.grey[300];
+  static Color lightGrey = Color.fromRGBO(238, 238, 238, 1);
+  static Color darkGray = Color.fromRGBO(87, 87, 87, 1);
+  static Color black = Color.fromRGBO(57, 57, 57, 1);
+}
